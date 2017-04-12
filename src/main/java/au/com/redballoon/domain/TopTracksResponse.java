c3368ce14ad85f48cb67d68267bd46d5323dca73
rodealmeida <rodealmeida@gmail.com>
2017-04-12 17:22:59 +1200

    Add top songs page by artist

--- src/main/java/au/com/redballoon/domain/TopTracksResponse.java
+++ src/main/java/au/com/redballoon/domain/TopTracksResponse.java
@@ -2,15 +2,21 @@
 package au.com.redballoon.domain;
 
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
 /**
  * Created by rodealmeida on 11/04/2017.
  */
+@JsonIgnoreProperties(ignoreUnknown = true)
+@JsonInclude(JsonInclude.Include.NON_NULL)
 public class TopTracksResponse
 {
 
     private TopTracks topTracks;
+
+    private String errorMessage;
 
     @JsonProperty("toptracks")
     public TopTracks getTopTracks() {
@@ -19,6 +25,18 @@ public class TopTracksResponse
 
     public void setTopTracks(TopTracks topTracks) {
         this.topTracks = topTracks;
+    }
+
+    @JsonProperty("errorMessage")
+    public String getErrorMessage()
+    {
+        return errorMessage;
+    }
+
+    @JsonProperty("message")
+    public void setErrorMessage(String errorMessage)
+    {
+        this.errorMessage = errorMessage;
     }
 
 }
