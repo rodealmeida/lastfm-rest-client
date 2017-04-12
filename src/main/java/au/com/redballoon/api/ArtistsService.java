243fc59bcb78147f0b8e90972dafc25cec3f696c
rodealmeida <rodealmeida@gmail.com>
2017-04-12 15:26:04 +1200

    Add top artist by country page with pagination

--- src/main/java/au/com/redballoon/api/ArtistsService.java
+++ src/main/java/au/com/redballoon/api/ArtistsService.java
@@ -1,14 +1,11 @@
 package au.com.redballoon.api;
 
 import javax.inject.Inject;
-import javax.servlet.ServletException;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.QueryParam;
-
-import java.io.IOException;
 
 import au.com.redballoon.domain.TopArtistsResponse;
 import au.com.redballoon.service.LastFmClient;
@@ -16,7 +13,7 @@ import au.com.redballoon.service.LastFmClient;
 /**
  * Created by rodealmeida on 11/04/2017.
  */
-@Path("/api/artists")
+@Path("/")
 @Consumes({ "application/json" })
 @Produces("application/json")
 public class ArtistsService
@@ -26,7 +23,7 @@ public class ArtistsService
     private LastFmClient lastFmRestClient;
 
     @GET
-    @Path("/top")
+    @Path("/artists/top")
     public TopArtistsResponse getTopArtistsByCountry(@QueryParam("country") String country,
                                                      @QueryParam("pageSize") Integer pageSize,
                                                      @QueryParam("page") Integer page)
