570c1ec6b8b3c26c060b0e0e531877daf38010fc
rodealmeida <rodealmeida@gmail.com>
2017-04-12 18:02:07 +1200

    Minor improvements and tidy up

--- src/main/java/au/com/redballoon/service/LastFmClient.java
+++ src/main/java/au/com/redballoon/service/LastFmClient.java
@@ -8,7 +8,25 @@ import au.com.redballoon.domain.TopTracksResponse;
  */
 public interface LastFmClient
 {
+    /**
+     * Finds the top artists for a given country.
+     *
+     * @param country to find the top artists for.
+     * @param pageSize number of items to be retrieved.
+     * @param page number to fetch.
+     *
+     * @return the top artists found by country.
+     */
     TopArtistsResponse getArtistsByCountry(String country, Integer pageSize, Integer page);
 
+    /**
+     * Finds the top songs for a given artist.
+     *
+     * @param artistName to find songs for.
+     * @param pageSize number of items to be retrieved.
+     * @param page number to fetch.
+     *
+     * @return the top songs found for an artist.
+     */
     TopTracksResponse getTopSongByArtist(String artistName, Integer pageSize, Integer page);
 }
